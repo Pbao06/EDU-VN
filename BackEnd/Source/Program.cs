8b839848748c6d527db3ca69c86a4d4bc3070c80
Phan Gia Bảo <phangia223@gmail.com>
2026-07-30 20:56:28 +0700

    dd

--- BackEnd/Source/Program.cs
+++ BackEnd/Source/Program.cs
@@ -19,15 +19,12 @@ namespace Source
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-            {
-                Args=args
-            });
+            var builder = WebApplication.CreateBuilder(args);
             // Tắt hoàn toàn việc theo dõi file appsettings.json để tránh tràn giới hạn inotify trên Render
             builder.Configuration.Sources.Clear();
             builder.Configuration
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
             .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false)
             .AddEnvironmentVariables();
 
